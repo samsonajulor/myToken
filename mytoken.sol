45f020cb92df49c68dfc99aefacec70111802478
samsonajulor <84925832+samsonajulor@users.noreply.github.com>
2023-11-12 10:12:55 +0100

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -18,8 +18,8 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-    string public tokenName = "Samson";
-    string public tokenAbbrv = "SAM";
+    string public tokenName = "SAMSON TOKEN";
+    string public tokenAbbrv = "SMT";
     uint public totalSupply = 0;
 
     // mapping variable here
